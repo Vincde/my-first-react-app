4ba914141ad8e5e2ecabb8d5859ba2cfa68bc4f9
Vincde <145802178+Vincde@users.noreply.github.com>
2025-03-02 11:31:58 +0100

    Playing around with react pt1

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -2,4 +2,15 @@ function Greeting() {
     return <h1>&quot; I swear by my pretty floral bonnet, i will end you.&quot;</h1>
   }
 
-  export default Greeting;
+  function MyFavoriteFood() {
+    return (<>
+    <h1>My favorite foods are: <br /></h1>
+    <div>Pizza</div>
+    <div>Pasta</div>
+    <div>Potatoes</div>
+
+    </>)
+
+  }
+
+  export {Greeting, MyFavoriteFood};
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 /* import App from './App.jsx' */
-import Greeting from './Greeting.jsx'
+import {Greeting, MyFavoriteFood} from './Greeting.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Greeting />
+    <Greeting></Greeting>
+    <MyFavoriteFood></MyFavoriteFood>
   </StrictMode>,
 )
