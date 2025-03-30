520e65930269992c55ac78987478ae9839b86976
Vincde <145802178+Vincde@users.noreply.github.com>
2025-03-30 17:09:46 +0200

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import RecipeList from './aaa';
+
 import './App.css'
+import { sculptureList } from './data';
 
 function App() {
 
+  let index = 0;
+
+  function handleClick() {
+    index +=1 ;
+  }
+
+  let sculpture = sculptureList[index];
+
   return(
-    <div>
-      <RecipeList></RecipeList>
-    </div>
-  );
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+    </>
+  )
 }
 
-export default App
+export default App;
