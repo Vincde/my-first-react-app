f5c30021f747c39f60088fff28ef0c1c6d3ada04
Vincde <145802178+Vincde@users.noreply.github.com>
2025-03-30 22:04:15 +0200

    aarre

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
 
 import './App.css'
 import { sculptureList } from './data';
+import { useState } from 'react';
 
 function App() {
 
-  let index = 0;
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore]  = useState(false);
 
-  function handleClick() {
-    index +=1 ;
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
 
   return(
     <>
-      <button onClick={handleClick}>
+      <button onClick={handleNextClick}>
         Next
       </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+          ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
     </>
-  )
+  );
 }
 
 export default App;
