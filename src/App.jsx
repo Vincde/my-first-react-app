88c41184fdfe948818a918a13ea902e2b12aaac3
Vincde <145802178+Vincde@users.noreply.github.com>
2025-04-01 23:58:11 +0200

    asdfkje

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
 
-import './App.css'
-import { sculptureList } from './data';
-import { useState } from 'react';
+import { useState } from "react";
+import "./App.css";
+
+const COLORS = ["pink", "green", "blue", "yellow", "purple"];
 
 function App() {
+  const [nTimes, setNTimes] = useState(0);
+  const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
 
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore]  = useState(false);
+  const onButtonClick = (color) => () => {
+    if(backgroundColor !== color){
+      setNTimes(nTimes + 1);
+    }
+    
 
-  function handleNextClick() {
-    setIndex(index + 1);
-  }
+    setBackgroundColor(color);
+    
+  };
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
 
-  let sculpture = sculptureList[index];
-
-  return(
-    <>
-      <button onClick={handleNextClick}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-          ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? 'Hide' : 'Show'} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.alt} />
-    </>
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundColor,
+      }}
+    >
+      {COLORS.map((color) => (
+        <button
+          type="button"
+          key={color}
+          onClick={onButtonClick(color)}
+          className={backgroundColor === color ? "selected" : ""}
+        >
+          {color}
+        </button>
+      ))}
+      <div>
+        Number of Times : {nTimes}
+      </div>
+    </div>
   );
 }
 
