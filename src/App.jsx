f1bc7e73188bfc138b8a80e19d8d0120ce68594c
Vincde <145802178+Vincde@users.noreply.github.com>
2025-04-21 12:53:48 +0200

    &&

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,18 @@ import "./App.css";
 
 
 function Person() {
-  const [person, setPerson] = useState({ name: "John", age: 100 });
   const [firstName, setfirstName] = useState("");
   const [lastName, setlastName] = useState("");
 
   const fullName = firstName + ' ' + lastName;
 
-  const handleIncreaseAge = () => {
-    console.log("in handleIncreaseAge (before setPerson call): ", person);
-    setPerson({ ...person, age: person.age + 1 });
-    // we've called setPerson, surely person has updated?
-    console.log("in handleIncreaseAge (after setPerson call): ", person);
-  };
 
-  // this console.log runs every time the component renders
-  // what do you think this will print?
-  console.log("during render: ", person);
 
   return (
     <>
       <CustomInput name={firstName} onChange={(e) => setfirstName(e.target.value)}  typeofName={"firstname"}></CustomInput>
       <CustomInput name={lastName} onChange={(e) => setlastName(e.target.value)} typeofName={"lastname"}></CustomInput>
       <h1>{fullName}</h1>
-      <h2>{person.age}</h2>
-      <button onClick={handleIncreaseAge}>Increase age</button>
     </>
   );
 }
