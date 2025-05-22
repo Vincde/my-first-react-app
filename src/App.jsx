37da335f37714e434b87f210ed5cdadf4d7c3d35
Vincde <145802178+Vincde@users.noreply.github.com>
2025-05-22 10:20:08 +0200

    %%

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 
 
-function App({todos, filter}) {
-  const [newTodo, setNewTodo] = useState('');
+function App() {
 
-  const visibleTodos = useMemo(() => {
-    return getFilteredTodos(todos, filter);
-  }, [todos, filter]);
+
+  const [value, setValue] = useState('');
+  const [count, setCount] = useState(-1);
+
+  useEffect(() => setCount(count + 1), [value]);
+
+  const onChange = ({ target }) => setValue(target.value);
+
+  return (
+    <div>
+      <input type="text" value={value} onChange={onChange} />
+      <div>Number of changes: {count}</div>
+    </div>
+  );
 }
+
+
 
 
 export default App;
